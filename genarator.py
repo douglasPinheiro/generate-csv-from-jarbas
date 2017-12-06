ac229bd19a4c00304d5e0ee24caaf026cb98a525
douglasPinheiro <douglas-nba2999@hotmail.com>
2017-12-06 16:39:05 -0200

    initial script

---
+++ genarator.py
@@ -0,0 +1,26 @@
+import requests, json
+
+def run(year, month):
+    has_next = True
+    url = "https://jarbas.serenatadeamor.org/api/chamber_of_deputies/reimbursement/?year=%s&month=%s" % (year, month)
+
+    while(has_next):
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise Exception(response.content)
+
+        response_obj = json.loads(response.content)
+        
+        if response_obj['next']:
+            url = response_obj['next']
+        else:
+            has_next = False
+
+        for result in response_obj['results']:
+            line = "%s,%s,%s,%s,%s\n" % (result['congressperson_name'], result['subquota_description'].replace(',', ''), result['party'], result['state'], result['document_value'])
+
+            with open('csvfile.csv','a') as file:
+                file.write(line)
+
+run(2017, 10)
