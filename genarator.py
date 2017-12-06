a95bcfe4f5707184fc2f4762c66105f3482f115c
douglasPinheiro <douglas-nba2999@hotmail.com>
2017-12-06 17:22:23 -0200

    add file result to source control

--- genarator.py
+++ genarator.py
@@ -1,13 +1,15 @@
-import requests, json
+import requests, json, time
 
 def run(year, month):
     has_next = True
-    url = "https://jarbas.serenatadeamor.org/api/chamber_of_deputies/reimbursement/?year=%s&month=%s" % (year, month)
+    #url = "https://jarbas.serenatadeamor.org/api/chamber_of_deputies/reimbursement/?year=%s&month=%s" % (year, month)
+    url = "https://jarbas.serenatadeamor.org/api/chamber_of_deputies/reimbursement/?limit=7&month=10&offset=945&year=2017"
 
     while(has_next):
         response = requests.get(url)
 
         if response.status_code != 200:
+            print(url)
             raise Exception(response.content)
 
         response_obj = json.loads(response.content)
